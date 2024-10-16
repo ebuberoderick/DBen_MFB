0f0cbcb84b6072f5b5285b82588bd2a4b36314db
ebuberoderick <ebuberoderick2@gmail.com>
2024-10-16 21:31:24 +0100

    updated UI

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,35 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import Feather from '@expo/vector-icons/Feather';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+
+
+const _layout = () => {
+    return (
+        <Tabs initialRouteName='home' screenOptions={{ headerShown: false }}>
+            <Tabs.Screen name='home' options={{
+                title:"Home",
+                tabBarIcon: () => <Feather name="home" size={24} />
+            }} />
+
+            <Tabs.Screen name='cards' options={{
+                title:"Card",
+                tabBarIcon: () => <AntDesign name="creditcard" size={24} />
+            }} />
+
+            <Tabs.Screen name='transactions' options={{
+                title:"Transaction",
+                tabBarIcon: () => <FontAwesome5 name="poll" size={24} />
+            }} />
+
+            <Tabs.Screen name='profiles' options={{
+                title:"Profile",
+                tabBarIcon: () => <FontAwesome5 name="user-alt" size={24} />
+            }} />
+        </Tabs>
+    )
+}
+
+export default _layout
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,34 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { Tabs } from 'expo-router'
-import Feather from '@expo/vector-icons/Feather';
-import AntDesign from '@expo/vector-icons/AntDesign';
-import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
-
+import { Stack } from 'expo-router'
 
 const _layout = () => {
     return (
-        <Tabs initialRouteName='/home' screenOptions={{ headerShown: false }}>
-            <Tabs.Screen name='home' options={{
-                title:"Home",
-                tabBarIcon: () => <Feather name="home" size={24} />
-            }} />
-
-            <Tabs.Screen name='cards' options={{
-                title:"Card",
-                tabBarIcon: () => <AntDesign name="creditcard" size={24} />
-            }} />
-
-            <Tabs.Screen name='transactions' options={{
-                title:"Transaction",
-                tabBarIcon: () => <FontAwesome5 name="poll" size={24} />
-            }} />
-
-            <Tabs.Screen name='profiles' options={{
-                title:"Profile",
-                tabBarIcon: () => <FontAwesome5 name="user-alt" size={24} />
-            }} />
-        </Tabs>
+        <Stack initialRouteName="(tabs)">
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
     )
 }
 
